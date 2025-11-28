d9bcc72a5a1fbe350a0021b6bbc0354f393ae5b6
Minksea <lmk.crimea@gmail.com>
2025-11-28 19:21:22 +0300

    Important changes made!

--- src/main/java/com/minksea/thepalehunger/ThePaleHunger.java
+++ src/main/java/com/minksea/thepalehunger/ThePaleHunger.java
@@ -1,5 +1,6 @@
 package com.minksea.thepalehunger;
 
+import com.minksea.thepalehunger.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -21,6 +22,8 @@ public class ThePaleHunger {
 
     public ThePaleHunger(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
--- src/main/java/com/minksea/thepalehunger/item/ModItems.java
+++ src/main/java/com/minksea/thepalehunger/item/ModItems.java
@@ -2,10 +2,19 @@ package com.minksea.thepalehunger.item;
 
 import com.minksea.thepalehunger.ThePaleHunger;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ThePaleHunger.MOD_ID);
+
+    public static final RegistryObject<Item> ANCIENT_GHOULIFIED_ORE = ITEMS.register("Ghoulified Ore",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
